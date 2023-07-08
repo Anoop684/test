d8b0d5a2adc687b3a902f2a09a7c2e259f5a1aa3
Sinojgeorge <sinojgeo007@gmail.com>
2023-07-08 23:06:26 +0530

    Palindrome commit

--- public class RegistrationControllerTests.cs
+++ public class RegistrationControllerTests.cs
@@ -68,5 +68,45 @@ public class RegistrationControllerTests
     {
     console.wrie("registration");
     }
+    
+    //Palindrome
+
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (IsPalindrome(number))
+            {
+                Console.WriteLine("The number is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The number is not a palindrome.");
+            }
+        }
+
+        static bool IsPalindrome(int number)
+        {
+            int originalNumber = number;
+            int reverse = 0;
+
+            while (number > 0)
+            {
+                int remainder = number % 10;
+                reverse = (reverse * 10) + remainder;
+                number = number / 10;
+            }
+
+            return originalNumber == reverse;
+        }
+    }
+
+   
+    
      
 }
